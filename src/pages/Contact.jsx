bdf0022273e8db2874916771f889fac3b131e9e9
GabyRosas <fleuryrosas@gmail.com>
2024-06-20 19:02:48 +0200

    Renormalize all the line endings

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,28 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
 const Contact = () => {
-    return (
-        <>
-            <h1>Contact page</h1>
-        </>
-    )
-}
+     /*const { register, handleSubmit, getValues } = useForm();
+      const onSubmit = handleSubmit((values) => {
+        alert('Form submit: ' + JSON.stringify(values));
+      });*/
+    const onSubmit = () => {
+                alert('Form submit: ');
+              };
+        
+  return (
+    <>
+      <h1>Create an account</h1>
+      <form onSubmit={onSubmit}>
+        <input placeholder='First name'/>
+        <input placeholder='Second name'/>
+        <input placeholder='Email'/>
+        <input placeholder='Password' type='password'/>
+        <input placeholder='Confirm password' type='password'/>
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  );
+};
 
 export default Contact;
